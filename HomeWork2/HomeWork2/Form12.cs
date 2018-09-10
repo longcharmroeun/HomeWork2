47bc0fbb4d9ca1401658f5be491036f6c94af866
long charmroeun <longcharmroeun@gmail.com>
2018-09-10 21:40:25 +0700

    add new homework

---
+++ HomeWork2/HomeWork2/Form12.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HomeWork2
+{
+    public partial class Form12 : Form
+    {
+        public string[] text = new string[20];
+        public int i = 0;
+        public Form12()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form6 form6 = new Form6();
+            form6.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Add(openFileDialog1.SafeFileName);
+                text[i] = File.ReadAllText(openFileDialog1.FileName);
+                i++;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = text[listBox1.SelectedIndex];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.DefaultExt = "txt";
+            saveFileDialog1.Title = "listbox";
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.CreateNew))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    sw.WriteLine(text[listBox1.SelectedIndex]);
+                }
+            }
+        }
+    }
+}
